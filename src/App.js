cdb039c16f56b50969f86dbb16d47e11e41d7b9c
kurotamashi30 <kurotamashi30@gmail.com>
2024-02-20 23:31:34 +0900

    add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 function App() {
   const [inputValue, setInputValue] = useState('');
+  const [harNumber,setHardNumber]=useState(1);
+  const hardSum = hardCalculate(hardNumber);
   const [list, setList] = useState(['ご飯食べる', 'コーディングする']);
 
   const addToList = () => {
@@ -11,9 +13,21 @@ function App() {
     });
     setInputValue(''); // Clears the input after adding to the list.
   };
+  const hardCalculate=(number)=>{
+    console.log('計算');
+    for (let i= 0; i<99999999; i++){
+
+    }return number+10000;
+  }
 
   return (
     <>
+    <input
+     type= "number"
+     value={hardNumber}
+     onChange={(e)=> setMardNumber(perseInt(e.target.value))}
+    />
+    <span> + 10000={hardSum}</span>
       <input
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
